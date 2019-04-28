d050c79e72ab8b29f35c6121a4b747ceff8cd43c
tirupathiraochinka <tirupathirao.chinka@gmail.com>
2019-04-28 10:15:01 -0700

    node module commit

---
+++ node_modules/@syncfusion/ej2-calendars/dist/global/index.d.ts
@@ -0,0 +1,5 @@
+import * as _calendars from '@syncfusion/ej2-calendars';
+
+export declare namespace ej {
+    const calendars: typeof _calendars;
+}
---
+++ node_modules/@syncfusion/ej2-ng-calendars/src/calendar/index.js
@@ -0,0 +1,4 @@
+export { CalendarComponent } from './calendar.component';
+export { CalendarModule } from './calendar.module';
+export { CalendarAllModule } from './calendar-all.module';
+//# sourceMappingURL=index.js.map
